f2bc1a637c64af5fd2af85bdebb44d652c2c49d3
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2025-03-07 23:56:42 +0600

    Notice Section, Moving through right to left

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
-import HeroSection from './components/heroSection'
 import Header from './components/header'
 import ImageCarousel from './components/imageCarousel'
+import Notice from './components/Notice'
+
 
 
 function App() {
   return (
     <div className="w-full"> 
        <Header />
+       <Notice />
       <ImageCarousel />
 
        {/* <HeroSection /> */}
