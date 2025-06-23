cc6df934d179d87af4151606ae01f64ac3d1e22c
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2025-06-24 01:22:45 +0600

    Add TeachersPage component with search and filter functionality

--- server/routes/teacher.routes.js
+++ server/routes/teacher.routes.js
@@ -4,13 +4,13 @@ import {
   getAllTeachersController,
   updateTeacherController,
   deleteTeacherController,
+  checkDuplicateTeacherController,
 } from '../controllers/teacher.controller.js';
 
 const router = express.Router();
-
 router.post('/addTeacher', addTeacherController);
 router.get('/', getAllTeachersController);
 router.put('/updateTeacher/:id', updateTeacherController);
 router.delete('/deleteTeacher/:id', deleteTeacherController);
-
+router.get('/checkDuplicate', checkDuplicateTeacherController);
 export default router;
